0df1e308ca1d25b3883de6f541ecea453e4a17d2
Adrian Knapp <adrknapp@gmail.com>
2021-12-07 16:16:37 -0300

    Added html and css on index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header>
+      <div className={styles.container}>
+        <Link href="/">
+          <a className={styles.logo}>
+            <Image src="/logo.svg" alt="logo" layout="fill" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 
+import { FiUser, FiCalendar } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +25,28 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <main className={styles.container}>
+      <div className={styles.post}>
+        <h2>Como utilizar Hooks</h2>
+        <p>Pensando em sincronizalção em vez de ciclos de vida</p>
+        <div className={styles.info}>
+          <div className={styles.date}>
+            <FiCalendar className={styles.icon} />
+            15 Mar 2021
+          </div>
+          <div className={styles.author}>
+            <FiUser className={styles.icon} />
+            Joseph Oliveira
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-
-//   // TODO
-// };
+/* export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query('TODO');
+}; */
